09da8e8d69bfc4fcd861c8f1b11159b463ed513c
jon.munoa <jmunoa91@gmail.com>
2017-01-30 16:12:11 +0100

    Añadido método de extensión al tipo Datetime

---
+++ extensionmethods/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionmethods
+{
+   public static class ExtensionMethods
+    {
+        public static DateTime Tomorrow(this DateTime date)
+        {
+            return date.AddDays(1);
+        }
+    }
+}
---
+++ extensionmethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace extensionmethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Mañana es {DateTime.Now.Tomorrow()}");
+            Console.ReadLine();
+        }
+    }
+}
